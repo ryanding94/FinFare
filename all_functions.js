72cc6e365110b9ba0276140ab8b41b68bfc907a2
Andersen <kendra.andersen@parivedasolutions.com>
2018-09-18 15:08:47 -0500

    Updates to link pages together

---
+++ all_functions.js
@@ -0,0 +1,28 @@
+function register_lunch() {
+    // Should do logic to register someone for the event
+    location.href = "homepage.html";
+}
+
+function cancel_lunch() {
+    // Should do logic to remove user from the event
+    location.href = "homepage.html";
+}
+
+function go_to_homepage() {
+    // Just sends the user to another page
+    location.href = "homepage.html";
+}
+
+function go_to_profile() {
+    // Sends user to their profile
+}
+
+function add_meal() {
+    // Sends user to create meal for today
+    location.href = "register-today.html";
+}
+
+function edit_meal() {
+    // Sends user to edit meal
+    location.href = "register-today.html";
+}

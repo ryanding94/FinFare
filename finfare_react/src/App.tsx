0f4bafc8aa0d123ddacad38d16c2efc819b59742
Andersen <kendra.andersen@parivedasolutions.com>
2018-09-20 11:32:29 -0500

    Add routing to the project

--- finfare_react/src/App.tsx
+++ finfare_react/src/App.tsx
@@ -4,16 +4,16 @@ import {MealUpdate} from './MealUpdate';
 import {NavBar} from './Navbar'; 
 import {Profile} from './Profile'; 
 
-interface IPageState{
+interface IAppState{
   page: number
   // 0 is homepage
   // 1 is my profile
   // 2 is add a meal
 }
 
-class App extends React.Component<{}, IPageState> {
-  constructor(p:any) {
-    super(p); 
+class App extends React.Component<{}, IAppState> {
+  constructor(props:any) {
+    super(props); 
     this.state = {
       page: 0
     }

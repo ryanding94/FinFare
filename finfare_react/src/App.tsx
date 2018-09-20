3fb0dfee3db448eb66418a6ffcfcd19f53b229e1
Andersen <kendra.andersen@parivedasolutions.com>
2018-09-20 09:17:41 -0500

    Add NavBar to react app

--- finfare_react/src/App.tsx
+++ finfare_react/src/App.tsx
@@ -1,14 +1,27 @@
 import * as React from 'react';
-import {NavBar} from './Navbar'; 
 import {MealUpdate} from './MealUpdate';
+import {NavBar} from './Navbar'; 
 
-class App extends React.Component {
+interface IPageState{
+  page: number
+  // 0 is homepage
+  // 1 is my profile
+  // 2 is add a meal
+}
+
+class App extends React.Component<{}, IPageState> {
+  constructor(p:any) {
+    super(p); 
+    this.state = {
+      page: 0
+    }
+  }  
   public render() {
     return (
       // Add the Navigation Bar
       <div>
-      <MealUpdate />
-      <NavBar />
+        <NavBar pageNum={this.state.page}/>
+        <MealUpdate />
       </div>
     );
   }
--- finfare_react/src/Navbar.tsx
+++ finfare_react/src/Navbar.tsx
@@ -1,16 +1,16 @@
 import * as React from 'react'; 
 
 interface INavBarState {
-    value: number; 
+    pageNum: number; 
     // 0 is homepage
     // 1 is my profile
     // 2 is add a meal
 }
 
-export class NavBar extends React.Component<{}, INavBarState> {
-    public changePage = () => {
-        // Any button leads to homepage right now
-        this.setState({value: 0})
+export class NavBar extends React.Component<INavBarState> {
+    public onUpdatePage = () => {
+        // Change the page on the nav bar
+        this.setState({pageNum: 0})
     }
     
     public render() {
@@ -26,13 +26,13 @@ export class NavBar extends React.Component<{}, INavBarState> {
                 <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
                     <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
                         <li className="nav-item active">
-                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                            <a className="nav-link" href="#" onClick={this.onUpdatePage}>Home <span className="sr-only">(current)</span></a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">My Profile</a>
+                            <a className="nav-link" href="#" onClick={this.onUpdatePage}>My Profile</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Add a new meal</a>
+                            <a className="nav-link" href="#" onClick={this.onUpdatePage}>Add a new meal</a>
                         </li>
                     </ul>
                 </div>        
